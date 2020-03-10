b90f32c7dd874a65d5c661075cc6bc998e56f0ab
Ramzi Tannous <rtannous@twistbioscience.com>
2020-03-10 10:51:32 +0200

    fix style & package.json

--- example/src/Component.tsx
+++ example/src/Component.tsx
@@ -1,6 +1,12 @@
-import { Button, Text, View } from 'react-native';
+import { Button, Text, View, ViewStyle } from 'react-native';
 import * as React from 'react';
 import { useModal } from 'react-native-modal-hook';
+
+const containerStyle: ViewStyle = {
+  flex: 1,
+  justifyContent: 'center',
+  alignItems: 'center',
+};
 
 export function TestComponent() {
   const { show } = useModal(() => {
@@ -8,7 +14,7 @@ export function TestComponent() {
   });
 
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+    <View style={containerStyle}>
       <Button onPress={() => show('hello')} title={'Open Modal'} />
     </View>
   );
